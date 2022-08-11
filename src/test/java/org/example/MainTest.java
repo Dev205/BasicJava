7e81537fe47715ad070848bf3b6de161981a5431
devin <devt205@gmail.com>
2022-08-11 10:17:54 +0100

    Added tests

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -3,10 +3,23 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 public class MainTest {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testSuccess() {
+        assertThat("String").isEqualTo("String");
+    }
+
+    @Test
+    void testFails() {
+        assertThat("String").isEqualTo("A");
     }
 }
